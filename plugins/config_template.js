5c9d266b77c31658f882db10eaba1a3fe08de130
matthew <themattarse@gmail.com>
2013-11-27 11:32:27 -0600

    adding purchase_acceleration

--- plugins/config_template.js
+++ plugins/config_template.js
@@ -16,13 +16,16 @@ exports.email = {
 exports.trading = {
   maximum_$_per_deal: 15,               // Maximum allowed $ per deal / trade / hand
   maximum_investment: 100,              // Maximum total $ allowed to invest in BTC
-  bid_alignment: 0.999,                 // Bid align for competitive edge when placing bids 
+  bid_alignment: 0.998,                 // Bid align for competitive edge when placing bids 
                                         // EXAMPLE: BTC price is $600, to buy, we would place order at: 600 / 0.999 = 600.6
                                         
   max_number_of_deals_per_trader: 3,    // How many deals can a trader manage
-  momentum_time_span: 5*60*1000,        // Set momentum time span to x minutes (change the first number, that is minutes)
-  greed: 0.05                           // What upside does the trader look for?
+  momentum_time_span: 3*60*1000,        // Set momentum time span to x minutes (change the first number, that is minutes)
+  greed: 0.05,
+  purchase_acceleration: 1.00            // EXAMPLE: If bought 1 BTC for $600, with greed at 0.05, it will sell for $630 = 600*(1+0.05
+                                        //upbid - allowing to buy at a certain percentage over daily medium                          // What upside does the trader look for?
                                         // EXAMPLE: If bought 1 BTC for $600, with greed at 0.05, it will sell for $630 = 600*(1+0.05)
+                                         //over_middle - allowing to buy at a certain percentage over daily medium 1.00 is no increase
 };
 
 // Google authentication keys
