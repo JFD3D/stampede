735859c008b058824115a0161fd89bfeac72792e
Peter Berezny <spontain@gmail.com>
2014-01-28 16:37:26 +0100

    Deal transfer script added

--- transfer_deals.js
+++ transfer_deals.js
@@ -1,8 +1,13 @@
 // Use as 'node transfer_deals.js NUMBEROFTRADERFROM NUMBEROFTRADERTO
 
-var rdb = redis.createClient(6379),
-    transfer_form = process.argv[2],
+var rdb = require("redis").createClient(6379),
+    transfer_from = process.argv[2],
     transfer_to = process.argv[3];
+
+if (transfer_from && transfer_to) {
+  transferDeals(transfer_from, transfer_to, console.log);
+}
+else console.log("Transfers not defined, process.argv:", process.argv);
 
 function transferDeals(from, to, callback) {
   var trader_to = "trader_"+to,
@@ -10,11 +15,13 @@ function transferDeals(from, to, callback) {
       book_from = "book_for_"+trader_from,
       book_to = "book_for_"+trader_to;
 
-  rdb.smembers(trader_from, function(error, deals) {
+  rdb.smembers(book_from, function(error, deals) {
     deals.forEach(function(deal_name) {
-      rdb.srem(trader_from, deal_name, function(error, response) { 
+      rdb.srem(book_from, deal_name, function(error, response) { 
         if (!error) 
-          rdb.sadd(trader_to, deal_name, function(error, response) { console.log("Assigned deal from "+trader_from+" to "+trader_to+" ("+deal_name+").")});
+          rdb.sadd(book_to, deal_name, function(error, response) { 
+            console.log("Assigned deal from "+trader_from+" to "+trader_to+" ("+deal_name+").")
+          });
         else
           console.log("Error removing deal ("+deal_name+"):", error, "from", trader_from);
       });
