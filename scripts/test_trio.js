ccb1cc77960a908a81a315ab4cf5a2eadc41acea
Peter Berezny <spontain@gmail.com>
2016-01-11 08:36:32 +0100

    Testing arbitrage

--- scripts/test_trio.js
+++ scripts/test_trio.js
@@ -10,7 +10,7 @@
   let btce    = BTCE(conf.credentials.btce.key, conf.credentials.btce.secret)
   let $amount = 100
   let fee     = 0.002
- 
+
   let tick    = (pair_str, done) => btce.ticker(pair_str, done)
   let calcOn  = (data) => {
     let btc_usd_last = data.btc_usd.last
@@ -23,15 +23,15 @@
     let ratio       = (($_per_btc / $amount * 100) - 100)
     let ratio_label = ratio.toFixed(2)
 
-    // console.log(`
-    //   btc_usd_last: ${btc_usd_last},
-    //   ltc_usd_last: ${ltc_usd_last},
-    //   ltc_btc_last: ${ltc_btc_last},
+    console.log(`
+      btc_usd_last: ${btc_usd_last},
+      ltc_usd_last: ${ltc_usd_last},
+      ltc_btc_last: ${ltc_btc_last},
 
-    //   ltc_per_$: ${ltc_per_$},
-    //   btc_per_ltc: ${btc_per_ltc},
-    //   $_per_btc: ${$_per_btc}
-    // `)
+      ltc_per_$: ${ltc_per_$},
+      btc_per_ltc: ${btc_per_ltc},
+      $_per_btc: ${$_per_btc}
+    `)
     let label = 'Profit / Loss: ' + ratio_label + '%'
     console.log(label[ratio > 100 ? 'rainbow' : 'red'])
   }
